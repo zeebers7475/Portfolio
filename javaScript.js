790252d758060337bce3c769fa2bf34b0289c4f9
zeebers7475 <zeebers7475@gmail.com>
2024-09-12 17:37:01 -0400

    Worked on JavaScript

---
+++ javaScript.js
@@ -0,0 +1,15 @@
+const button = document.getElementsByTagName('button');
+
+const showPic = async (event) => {
+    let list = document.getElementById('projects').getElementsByTagName('li');
+    const pic = document.getElementsByClassName('project-img');
+    let nextPic = 
+    console.log(event.target)
+   
+
+    //pic[0].style.display = 'block';
+};
+
+button[0].addEventListener('click', showPic);
+button[1].addEventListener('click', showPic);
+button[2].addEventListener('click', showPic);

9475463c2c02b8d27b6a76dd8cdc8ae5b71dd724
zeebers7475 <zeebers7475@gmail.com>
2024-09-13 01:38:43 -0400

    Finished

--- javaScript.js
+++ javaScript.js
@@ -1,15 +1,17 @@
-const button = document.getElementsByTagName('button');
+const buttons = document.querySelectorAll('button');
 
-const showPic = async (event) => {
-    let list = document.getElementById('projects').getElementsByTagName('li');
-    const pic = document.getElementsByClassName('project-img');
-    let nextPic = 
-    console.log(event.target)
-   
-
-    //pic[0].style.display = 'block';
+const toggleImage = (event) => {
+    let button = event.target;
+    let imageId = button.getAttribute('data-image-id');
+    const pic = document.getElementById(imageId)
+    
+    if(pic.style.display === 'none' || !pic.style.display) {
+        pic.style.display = 'block';
+    } else {
+        pic.style.display= 'none'
+    }
 };
 
-button[0].addEventListener('click', showPic);
-button[1].addEventListener('click', showPic);
-button[2].addEventListener('click', showPic);
+buttons.forEach((button) => {
+    button.addEventListener('click', toggleImage);
+});
